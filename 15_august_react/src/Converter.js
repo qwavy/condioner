ec3ba7f2e48784a12705dc4d9c2701a81eab1541
qwavy <97668069+qwavy@users.noreply.github.com>
2022-08-17 17:23:01 +0600

    dz
    добавил четнось чисел

--- 15_august_react/src/Converter.js
+++ 15_august_react/src/Converter.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-function Converter() {
-    // const [amount, setAmount] = useState('')
-    // const [res, setRes] = useState(0)
-    // const [chet,setChet] = useState('')
+function Converter({rus_eng}) {
+    const [amount, setAmount] = useState('')
+    const [res, setRes] = useState(0)
+    const [chet,setChet] = useState('')
     // // // console.log(amount)
     // // return (
     // //     <div className='container'>
@@ -17,70 +17,103 @@ function Converter() {
     // //         </div>
     // //     </div>
     // // )
-    // let sum = 0
-    // const handleCheck = () => {
-    //     if (amount % 2 === 0){
-    //         setChet('нечетное')
-    //     }else{
-    //         setChet('четное')
-    //     }
+    let sum = 0
+    const handleCheck = () => {
+        if (amount % 2 === 0){
+            setChet('нечетное')
+        }else{
+            setChet('четное')
+        }
 
-    //         for (let i = 0; i < amount.length; i++) {
-    //             sum += +amount[i]
-    //             setRes(sum)
-    //             // console.log(sum)
-    //         }
-    //     console.log(res)
-    //     // return (res)
+            for (let i = 0; i < amount.length; i++) {
+                sum += +amount[i]
+                setRes(sum)
+                // console.log(sum)
+            }
+        console.log(res)
+        // return (res)
 
-    // }
+    }
     // return (
-    //     <div className='container'>
-    //         <h1>Вычеслить сумму чисел</h1>
-    //         <input type="number" value={amount} onChange={event => setAmount(event.target.value)}></input>
-    //         <p>{res}</p>
-    //         <p>{chet}</p>
 
-    //         <button onClick={handleCheck}>click</button>
-    //     </div>
     // )
     const [russian, setRussian] = useState('')
+    // const [englsih, setEnglish] = useState('')
+    const [english,setEnglish] = useState('')
     const [result, setResult] = useState('')
 
-    const translate = () => {
-        // if(russian == 'привет'){
-        //     setResult('hello')
-        // }else{
-        //     alert('нет такого слова')
-        //     setResult('')
-
+    const translate_rus = () => {
+        // switch (russian) {
+        //     case 'привет':
+        //         setResult('hello')
+        //         break;
+        //     case 'пока':
+        //         setResult('bye')
+        //         break;
+        //     default:
+        //         alert('нет такого слова')
+        //         setResult('')
         // }
-        switch (russian) {
-            case 'привет':
-                setResult('hello')
+        for(let i = 0;i < rus_eng.length;i++){
+            if(rus_eng[i].rus == russian.toLowerCase()){
+                setResult(rus_eng[i].eng)
                 break;
-            case 'пока':
-                setResult('good bye')
+                
+            }else if(russian == ''){
+                console.log('введите значение')
+            }else{
+                console.log('нету такого слова')
                 break;
-            default:
-                alert('нет такого слова')
-                setResult('')
+
+            }
+        }
+
+    }
+    const translate_eng = () => {
+        for(let i = 0;i < rus_eng.length;i++){
+            if(rus_eng[i].eng == english.toLowerCase()){
+                setResult(rus_eng[i].rus)
+                break;
+
+                
+            }else if(english == ''){
+                console.log('введите значение')
+            }else{
+                console.log('нету такого слова')
+                break;
+            }
         }
     }
-    // const [russian,setRussian] = useState('')
     return (
         <div>
             <div>
                 <input type='text' value={russian} onChange={event => setRussian(event.target.value)} placeholder='русское'>
 
                 </input>
-                <button onClick={translate}>
-                    перевести
+
+                <button onClick={translate_rus}>
+                    перевести на англиский
+            </button>
+            <input type='text' value={english} onChange={event => setEnglish(event.target.value)} placeholder='англиское'>
+                </input>
+            <button onClick={translate_eng}>
+                    перевести на русский
             </button>
                 <p>
                     {result}
+                    {/* {rus_eng.map((rus) => <li>
+                        {rus}
+                    </li>)} */}
                 </p>
             </div>
+            <div className='container'>
+            <h1>Вычеслить сумму чисел</h1>
+            <input type="number" value={amount} onChange={event => setAmount(event.target.value)}></input>
+            <p>{res}</p>
+            <p>{chet}</p>
+
+            <button onClick={handleCheck}>click</button>
+        </div>
         </div>
     )
 }
